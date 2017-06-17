efa6d660a0a5643dfce7fb9e53f8fe25f9b44267
Robert Helmer <rhelmer@rhelmer.org>
2017-06-16 18:35:32 -0700

    basic http POST support, w/ hardcoded test request

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,49 @@ extern crate futures;
 extern crate hyper;
 extern crate tokio_core;
 
-use std::io::{self, Write};
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
 use futures::{Future, Stream};
+
 use hyper::Client;
+use hyper::{Method, Request};
+use hyper::header::{ContentLength, ContentType};
+
 use tokio_core::reactor::Core;
 
+#[derive(Serialize)]
+struct UpdateCheck {
+    product: &'static str,
+    version: i32,
+    platform: &'static str,
+    locale: &'static str,
+}
 
 fn main() {
     let mut core = Core::new().unwrap();
     let client = Client::new(&core.handle());
 
-    let uri = "http://localhost:8080/src/update.json".parse().unwrap();
-    let work = client.get(uri).and_then(|res| {
-        println!("Response: {}", res.status());
+    let check = UpdateCheck {
+        product: "your_app",
+        version: 666,
+        platform: "macOS",
+        locale: "en-US",
+    };
 
-        res.body().for_each(|chunk| {
-            io::stdout().write_all(&chunk).map(|_| ()).map_err(From::from)
-        })
+    let json_post = serde_json::to_string(&check).unwrap();
+    let uri = "http://localhost:8000/src/update.json".parse().unwrap();
+    let mut req = Request::new(Method::Post, uri);
+    req.headers_mut().set(ContentType::json());
+    req.headers_mut().set(ContentLength(json_post.len() as u64));
+    req.set_body(json_post);
+
+    let post = client.request(req).and_then(|res| {
+        println!("POST: {}", res.status());
+        res.body().concat2()
     });
 
-    core.run(work).unwrap();
+    core.run(post).unwrap();
 }
