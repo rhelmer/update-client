f506e369baa285b1900ab6f27c06ba8ebd00d7cd
Robert Helmer <rhelmer@rhelmer.org>
2017-06-19 14:19:14 -0700

    move example app into client repo

---
+++ examples/Rust/README.md
@@ -0,0 +1,23 @@
+# Example app for Application Update client
+
+This is a simple Rust app to exercise the [Application Update client](https://github.com/rhelmer/update-client).
+
+## Getting started
+
+### Dependencies
+
+You need to have an update server running. The default is the [Application Update server](https://github.com/rhelmer/update-server).
+
+A simple `cargo run` should be enough to get the server running on `localhost:8000`.
+
+Next, build the [Application Update client](https://github.com/rhelmer/update-client) by running `cargo build --release`. This app will look in `../../target/release/update_client` for the update client binary.
+
+### Build and run.
+
+`cargo run`
+
+The example app will attempt to spawn the update client as a child process.
+
+If updates are found, the update client will download them and notify the
+example app, which will "install" the update by copying it to the `./updates`
+directory.
---
+++ examples/Rust/src/main.rs
@@ -0,0 +1,43 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+use std::fs;
+use std::process::Command;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ResultMessage {
+    update_type: String,
+    download_path: String,
+}
+
+fn main() {
+    println!("\n-----------------------------------------------------\n");
+    println!("This is an example app to exercise the update client!\n");
+    println!("Checking for updates...\n");
+
+    let output = Command::new("../../target/release/update_client")
+        .output()
+        .expect("failed to execute process");
+
+    let json_str = String::from_utf8_lossy(&output.stdout);
+    let result_message: ResultMessage = serde_json::from_str(&json_str).unwrap();
+
+    println!("{} update is ready to apply at {}\n",
+        result_message.update_type,
+        result_message.download_path);
+
+    println!("Applying update for {}...", result_message.update_type);
+
+    fs::copy(result_message.download_path,
+        format!("./updates/{}", result_message.update_type)).unwrap();
+
+    println!("{} update has been applied in ./updates/{}\n",
+        result_message.update_type,
+        result_message.update_type);
+
+    println!("All done!");
+    println!("\n-----------------------------------------------------\n");
+
+}
